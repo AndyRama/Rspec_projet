1b3b42521c32fc52eafdb2e8459b253e50cf2e57
AndyRama <andyramaroson@gmail.com>
2021-08-31 22:16:29 +0200

    first test for post_spec.rb

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  context "comment association" do
+    it 'should increment comments_count' do
+      post = Post.new(name: 'First test')
+      post.comments << Comment.new(content:'demo')
+      post.save
+      expect(Comment.count).to eq(1)
+      expect(post.comments_count).to eq(1)
+    end
+  end
+    
+  end
+  
+  it 'should persist a post' do
+    Post.create(name: 'First test')
+    expect(Post.count).to eq(2)
+  end
 end
